ce9e5947fd8e0a7b04348c8da7deec8059327457
Kayxue <s0990006@gmail.com>
2024-09-16 18:11:46 +0800

    2024/09/16 Login Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,40 @@
-'use client'
+"use client";
 import axios from "../axiosInstance/axiosWithCredentials";
 import { useState } from "react";
 
 export default function Home() {
-  const [username,setUsername]=useState("")
-  const [password,setPassword]=useState("")
-  const [data,setData]=useState("")
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [data, setData] = useState("");
 
-  async function login(){
-    const result=await axios.post("http://localhost:3001/user/login",{username,password});
-    setData(JSON.stringify(result.data));
-  }
+	async function login() {
+		const result = await axios.post("http://localhost:3001/user/login", {
+			username,
+			password,
+		});
+		setData(JSON.stringify(result.data));
+	}
 
-  async function getProfile(){
-    const result=await axios.get("http://localhost:3001/user/profile").catch(() => ({data:""}));
-    setData(JSON.stringify(result.data));
-  }
+	async function getProfile() {
+		const result = await axios
+			.get("http://localhost:3001/user/profile")
+			.catch(() => ({ data: "" }));
+		setData(JSON.stringify(result.data));
+	}
 
-  return (
-    <>
-      <input value={username} onChange={(e) => setUsername(e.target.value)}></input>
-      <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
-      <button onClick={() => login()}>login</button>
-      <button onClick={() => getProfile()}>profile</button>
-      <p>{data}</p>
-    </>
-  );
+	return (
+		<>
+			<input
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+			></input>
+			<input
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			></input>
+			<button onClick={() => login()}>login</button>
+			<button onClick={() => getProfile()}>profile</button>
+			<p>{data}</p>
+		</>
+	);
 }
