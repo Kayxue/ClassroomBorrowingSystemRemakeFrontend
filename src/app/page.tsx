68738fb08a511ce267b7c3676d94b4e2edd252c8
Yew02 <zongyung6656@gmail.com>
2024-11-29 18:17:57 +0800

    add home page
    - delete dark mode prefer (global.css)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,30 @@
 "use client";
-import axios from "../axiosInstance/axiosWithCredentials";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [data, setData] = useState("");
+	const router = useRouter();
 
-	async function login() {
-		const result = await axios.post("http://localhost:3001/user/login", {
-			username,
-			password,
-		});
-		setData(JSON.stringify(result.data));
+	function goToLogin() {
+		router.push('/login');
 	}
 
-	async function getProfile() {
-		const result = await axios
-			.get("http://localhost:3001/user/profile")
-			.catch(() => ({ data: "Can't get any data" }));
-		setData(JSON.stringify(result.data));
-	}
-
-	async function logout() {
-		await axios.get("http://localhost:3001/user/logout");
-		setData("Logged out");
-	}
-
-	return (
-		<div className="space-x-2">
-			<input
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-				placeholder="username"
-				className="border-2 border-black border-solid rounded-xl pl-2 pr-2"
-			></input>
-			<input
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-				placeholder="password"
-				className="border-2 border-black border-solid rounded-xl pl-2 pr-2"
-			></input>
-			<button
-				className="bg-green-300 text-white rounded-2xl pl-2 pr-2 focus:ring-2 focus:ring-green-300"
-				onClick={() => login()}
-			>
-				login
-			</button>
-			<button
-				className="bg-blue-500  text-white rounded-2xl pl-2 pr-2 focus:ring-blue-300 focus:ring-2"
-				onClick={() => getProfile()}
-			>
-				profile
-			</button>
-			<button
-				className="bg-red-500 text-white rounded-2xl pl-2 pr-2 focus:ring-red-300 focus:ring-2"
-				onClick={() => logout()}
-			>
-				logout
-			</button>
-			<p>{data}</p>
+	return ( 
+		<div className="h-[30rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased">
+		<div className="max-w-2xl mx-auto p-4">
+		  <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+			教室借用系統
+		  </h1>
+		  <p></p>
+		  <p className="text-neutral-500 max-w-lg mx-auto my-2 text-base text-center relative z-10">
+		   歡迎來到教室借用系統，請敬請的使用吧！！！
+		  </p>
 		</div>
+		<button 
+			onClick={() => goToLogin()}
+			className="bg-blue-500 text-white rounded-2xl px-5 py-1 focus:ring-blue-300 focus:ring-2 "
+		>
+			login
+		</button>
+	  </div>
 	);
 }
