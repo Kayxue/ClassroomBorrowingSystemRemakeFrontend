eff4a20d89fd4883615422ce0206b69e757f81a5
Kayxue <s0990006@gmail.com>
2024-12-19 00:13:26 +0800

    Change checkbox style

--- src/components/RecordList.tsx
+++ src/components/RecordList.tsx
@@ -191,9 +191,10 @@ export default function BorrowRecords({ type, param }: BorrowRecordsProps) {
       </nav>
       {activeTab !== 1 && (
         <div className="mb-4 flex items-center">
-          <input type="checkbox" id="filterCheckbox" checked={filterBeforeToday} onChange={handleFilterChange} className="mr-2" />
-          <label htmlFor="filterCheckbox" className="text-sm text-gray-700">
-            過濾今天以前的資料
+          <label className="inline-flex items-center cursor-pointer">
+            <input type="checkbox" id="filterCheckbox" checked={filterBeforeToday} onChange={handleFilterChange} className="sr-only peer" />
+            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">過濾今天以前的資料</span>
           </label>
         </div>
       )}
